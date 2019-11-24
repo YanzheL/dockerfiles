d40457642df8785e2038d49319dcd4a1cb1cfc51
YanzheL <lee.yanzhe@yanzhe.org>
2019-11-24 22:24:59 +0800

    check health

---
+++ etcd/chk-health.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker exec -e ETCDCTL_API=3 \
+etcd \
+etcdctl \
+--cert /etc/etcd/pki/healthcheck-client.crt \
+--key /etc/etcd/pki/healthcheck-client.key \
+--cacert /etc/etcd/pki/ca.crt \
+--endpoints https://localhost:2379 endpoint status
