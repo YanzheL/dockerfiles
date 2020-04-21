680d15e71dcb0bb40a5cc825d701a1e71e3ce489
YanzheL <lee.yanzhe@yanzhe.org>
2020-04-22 04:20:08 +0800

    update

---
+++ jvm/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for JVM in openj9-jdk11 openj9-jre11 zing-11 ; do
+    docker build -t leeyanzhe/jvm:$JVM --build-arg URL=$(cat $JVM.url) . ;
+    docker push leeyanzhe/jvm:$JVM ;
+done
