519216949224487c76c15d7f7c83c53f1f410809
YanzheL <lee.yanzhe@yanzhe.org>
2021-05-04 14:46:14 +0800

    Update

--- xray/podman-xray-s.sh
+++ xray/podman-xray-s.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 podman create \
-    -d \
     --name xray-s \
     --net host \
     -v ./config.json:/etc/xray/config.json \
+    -v ./ssl:/etc/xray/ssl \
     --restart always \
     leeyanzhe/xray:latest
